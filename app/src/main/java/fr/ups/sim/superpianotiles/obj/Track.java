fee2b509fec579986d57c69a190c277d001ed8d5
Valérian Perez <valerian.perez@gmail.com>
2016-03-23 09:54:44 +0100

    Modif Tile et tentative de merge de tout

--- app/src/main/java/fr/ups/sim/superpianotiles/obj/Track.java
+++ app/src/main/java/fr/ups/sim/superpianotiles/obj/Track.java
@@ -6,6 +6,7 @@ import android.graphics.RectF;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
 /**
@@ -14,15 +15,29 @@ import java.util.Random;
 public class Track {
     private int tilesCount;
     private List<Tile> tiles;
+    private int contentWidth;
+    private int contentHeight;
 
-    public Track(int tilesCount){
+
+    public Track(int tilesCount, int contentWidth, int contentHeight){
 
         this.tilesCount = tilesCount;
         tiles = new LinkedList<>();
+        this.contentHeight = contentHeight;
+        this.contentWidth = contentWidth;
 
         for (int i = 0; i < tilesCount; i++){
-            //La position des tuiles n'est pas définit à leur création.
-            tiles.add(new Tile(Color.RED, Color.WHITE, "1", 40, null, null));
+
+            //Dépendent du random du quarterPosition
+            int rnd = QuarterPosition.randomPosition().ordinal();
+            int left = contentWidth * rnd/4;
+            int right = contentWidth * (rnd+1)/4;
+
+            //Dépendent de leur position dans la liste
+            int top = contentHeight * 1/4;
+            int bottom = contentHeight * 2/4;
+
+            tiles.add(new Tile(Color.RED, Color.WHITE, "1", 40, new Position(left, top, right, bottom), new RectF(left, top, right, bottom)));
         }
     }
 
@@ -35,18 +50,17 @@ public class Track {
      *
      * @return La liste des tuiles à afficher.
      */
-    public List<Tile> tilesDisplay(int contentWidth, int contentHeight){
-        List<Tile> tilesToDisplay = new ArrayList<>(4);
+    public ArrayList<Tile> tilesDisplay(){
+        ArrayList<Tile> tilesToDisplay = new ArrayList<>(4);
 
         //La première tuile de la liste est la tuile la plus basse à l'écran
-        for (int i = 4; i > 0; i--){
+        for (int i = 0; i < 4; i++){
             //Impossible de faire un poll...
             Tile t = tiles.get(0);
 
             //Dépendent du random du quarterPosition
-            int rnd = QuarterPosition.randomPosition().ordinal();
-            int left = contentWidth * rnd/4;
-            int right = contentWidth * (rnd+1)/4;
+            int left = t.getPositionTile().getLeft();
+            int right = t.getPositionTile().getRight();
 
             //Dépendent de leur position dans la liste
             int top = contentHeight * i/4;
@@ -54,7 +68,7 @@ public class Track {
 
             //Redéfinition de la position et de la forme.
             t.setPositionTile(new Position(left, top, right, bottom));
-            t.setTileForm(new RectF(left, top, right, bottom));
+            //t.setTileForm(new RectF(left, top, right, bottom));
 
             tilesToDisplay.add(t);
         }
